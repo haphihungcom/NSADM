ec64fa4a416aebc6304aaa202af46e4f1da51d22
Ha Phi Hung <haphihung55@gmail.com>
2020-06-06 21:41:39 +0700

    New exceptions

--- nsadm/exceptions.py
+++ nsadm/exceptions.py
@@ -1,22 +1,47 @@
 class NSADMError(Exception):
+    """NSADM general error.
+    """
+
+    pass
+
+
+class LoaderError(NSADMError):
+    """Loader plugin error.
+    """
+
     pass
 
 
 class DispatchAPIError(NSADMError):
+    """Dispatch API error.
+    """
+
     pass
 
 
-class DispatchUploader(NSADMError):
+class DispatchUpload(NSADMError):
+    """Dispatch upload error.
+    """
+
     pass
 
 
-class DispatchRendererError(NSADMError):
+class DispatchRenderError(NSADMError):
+    """Dispatch rendering error.
+    """
+
     pass
 
 
-class BBParserError(DispatchRendererError):
+class BBParsingError(DispatchRendererError):
+    """BBCode parsing errors.
+    """
+
     pass
 
 
 class TemplateRendererError(DispatchRendererError):
+    """Jinja template rendering errors.
+    """
+
     pass

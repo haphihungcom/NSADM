fe005810f0950f5b13e54db44dcf0951242e35cb
Ha Phi Hung <haphihung55@gmail.com>
2020-11-27 23:15:00 +0700

    Add credential manager object

--- nsadm/utils.py
+++ nsadm/utils.py
@@ -15,6 +15,32 @@ import bs4
 logger = logging.getLogger(__name__)
 
 
+class CredManager(collections.UserDict):
+    def __init__(self, cred_loader, dispatch_api):
+        self.cred_loader = cred_loader
+        self.dispatch_api = dispatch_api
+
+    def load_creds(self):
+        """Load all credentials from loader.
+        """
+
+        self.data = self.cred_loader.get_creds()
+
+    def __setitem__(self, nation_name, password):
+        """Add a new crendential with X-Autologin.
+
+        Args:
+            nation_name (str): Nation name
+            password (str): Password
+        """
+
+        x_autologin = self.dispatch_api.login(nation_name, password)
+        self.cred_loader.add_cred(nation_name, x_autologin)
+
+    def __delitem__(self, nation_name):
+        self.cred_loader.remove_cred(nation_name)
+
+
 def get_dispatch_info(dispatch_config):
     """Compose and return dispatch information
     for use as context in the template renderer.
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,6 +9,33 @@ import pytest
 from nsadm import utils
 
 
+class TestCredManager():
+    def test_load_creds(self):
+        mock_creds = {'nation1': '123456', 'nation2': '567890'}
+        mock_cred_loader = mock.Mock(get_creds=mock.Mock(return_value=mock_creds))
+        creds = utils.CredManager(mock_cred_loader, mock.Mock())
+
+        creds.load_creds()
+
+        assert creds['nation1'] == '123456'
+
+    def test_add_cred(self):
+        mock_cred_loader = mock.Mock(add_cred=mock.Mock())
+        mock_dispatch_api = mock.Mock(login=mock.Mock(return_value='123456'))
+        creds = utils.CredManager(mock_cred_loader, mock_dispatch_api)
+
+        creds['nation1'] = 'hunterprime'
+
+        mock_cred_loader.add_cred.assert_called_with('nation1', '123456')
+
+    def test_remove_cred(self):
+        mock_cred_loader = mock.Mock(remove_cred=mock.Mock())
+        creds = utils.CredManager(mock_cred_loader, mock.Mock())
+
+        del creds['nation1']
+
+        mock_cred_loader.remove_cred.assert_called_with('nation1')
+
 class TestGetDispatchInfo():
     def test_get_dispatch_info(self):
         dispatch_config = {'nation1': {'dispatch1': {'title': 'Test Title 1',
